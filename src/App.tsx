fd9202ac54f509febf78b7be8344026bc1222831
Aditya Khadkikar <guskhadad@student.gu.se>
2023-02-23 00:42:46 +0100

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          A  <code>Portfolio Website</code>  is being built here.
+          Aditya Khadkikar's  <code>Portfolio Website</code>  is being built here.
         </p>
         <a
           className="App-link"
