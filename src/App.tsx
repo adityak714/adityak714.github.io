96db1ecceadba6de1769c3ccafbeb971bc56cd28
Aditya Khadkikar <guskhadad@student.gu.se>
2023-02-23 04:43:04 +0100

    first header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header/>
+      <img src={logo} className="App-logo" alt="logo" />
         <h1>
           Currently in progress ...
         </h1>
         <p>
           Aditya Khadkikar's Portfolio Website is being built here.
         </p>
-      </header>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import '../index.css'
+
+function Header() {
+  return (
+    <header id="header-menu" style={{
+      backgroundImage: 'linear-gradient(to right, rgba(0, 60, 120, 0.85), rgba(0, 60, 120, 0.65))',
+      position: 'static',
+      padding: '10px 50px 10px 50px',
+      margin: '0px 0px 75px 0px',
+      textAlign: 'start',
+      fontSize: '1.5rem',
+      color: 'white',
+      fontFamily: 'Ubuntu',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    }}>
+      <p>
+        Aditya Khadkikar        
+      </p>
+      <div className="links" style={{
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <a href="mailto:guskhadad@student.gu.se" target={"_blank"} rel={'noreferrer'}>Email</a>
+        <a href="https://www.linkedin.com/in/aditya-k-8b9a00220/" target={"_blank"} rel={'noreferrer'}>LinkedIn</a>
+        <a href="https://github.com/adityak714" target={"_blank"} rel={'noreferrer'}>GitHub</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
